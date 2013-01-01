1f06c0a8c82b40f64024cc2d8f917b828d8edefa
wilfrem <wilfrem@gmail.com>
2013-01-01 23:45:20 +0900

    ユニットテストを追加

---
+++ MediaInterfacesUnitTest/VideoPropertyTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Wilfrem.HerculeMaker.MediaInterfaces;
+
+namespace MediaInterfacesUnitTest
+{
+    [TestClass]
+    public class VideoPropertyTest
+    {
+        VideoProperty property;
+        [TestInitialize]
+        public void SetUp()
+        {
+            property = new VideoProperty();
+            property.BitRate = 12345;
+            property.Format = VideoEncoding.H264;
+            property.Height = 11111;
+            property.Width = 12345;
+            property.Output = null;
+        }
+        [TestMethod]
+        public void TestClone()
+        {
+            var newProp = property.Clone();
+            Assert.AreEqual(property.BitRate, newProp.BitRate);
+            Assert.AreEqual(property.Format, newProp.Format);
+            Assert.AreEqual(property.Height, newProp.Height);
+            Assert.AreEqual(property.Output, newProp.Output);
+            Assert.AreEqual(property.Width, newProp.Width);
+        }
+    }
+}
